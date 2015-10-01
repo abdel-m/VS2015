a156ca7ae757b8b141509f45b7a35097c9708e96
Juma94 <julien.vdb001@gmail.com>
2015-10-01 16:49:25 +0200

    Test

---
+++ ConsoleApplicationLabo2/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationLabo2
+{
+    public abstract class Person
+    {
+        
+        public Person(String name, String lastName)
+        {
+            Name = name;
+            Lastname = lastName;
+        }
+        public string Lastname
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
